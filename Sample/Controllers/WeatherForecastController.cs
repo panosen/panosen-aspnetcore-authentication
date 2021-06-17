c253ce839a9a9ed72b29f87173e9932098966ebf
Harris Zhang <harriszhang@live.cn>
2021-06-17 10:06:59 +0800

    fix code issue.

--- Sample/Controllers/WeatherForecastController.cs
+++ Sample/Controllers/WeatherForecastController.cs
@@ -28,12 +28,10 @@ namespace Sample.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = index + 1
             })
             .ToArray();
         }
