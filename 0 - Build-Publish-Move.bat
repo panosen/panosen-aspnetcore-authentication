5ecf1cac6831f4c26a4928a80ed15e38e34c011d
hc_zhang <hc_zhang@trip.com>
2021-07-13 19:50:57 +0800

    update bat

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -4,9 +4,6 @@ dotnet restore
 
 dotnet build --no-restore -c Release
 
-dotnet nuget Panosen.AspNetCore.Authentication.Basic\bin\Release\Panosen.AspNetCore.Authentication.Basic.*.nupkg -s https://package.savory.cn/v3/index.json --skip-duplicate
-dotnet nuget Panosen.AspNetCore.Authentication.Header\bin\Release\Panosen.AspNetCore.Authentication.Header.*.nupkg -s https://package.savory.cn/v3/index.json --skip-duplicate
-
 move /Y Panosen.AspNetCore.Authentication.Basic\bin\Release\Panosen.AspNetCore.Authentication.Basic.*.nupkg D:\LocalSavoryNuget\
 move /Y Panosen.AspNetCore.Authentication.Header\bin\Release\Panosen.AspNetCore.Authentication.Header.*.nupkg D:\LocalSavoryNuget\
 
